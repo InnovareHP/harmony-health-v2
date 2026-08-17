e09e8011cbc8a10d0dc787afd7cd9be230f68c7a
redcloud442 <cloudred442@gmail.com>
2026-08-18 02:37:31 +0800

    remove market dialog

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,6 +1,5 @@
 import { CrisisBanner } from "@/src/index-page/index-page";
 import type { ReactNode } from "react";
-import DialogMarket from "../DialogMarket";
 import Footer from "../Footer";
 import Header from "../Header";
 
@@ -13,7 +12,7 @@ export default function MainLayout({ children, isIndexPage }: Props) {
       <Header /> {/* Only place where Header is rendered */}
       <section className="flex-grow relative">{children}</section>
       <Footer />
-      <DialogMarket />
+      {/* <DialogMarket /> */}
     </main>
   );
 }
